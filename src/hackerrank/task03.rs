19726b3465965679c57f04c86b96dd3da0c010be
СинковскийЕгор31 <synkovsky.yegor@e-u.edu.ua>
2026-06-11 14:26:24 +0300

    скріншоти для практичної номер 2

--- src/hackerrank/task03.rs
+++ src/hackerrank/task03.rs
@@ -1,1 +1,36 @@
-// https://www.hackerrank.com/challenges/staircase/problem
+#![allow(dead_code)]
+pub fn staircase_logic(n: i32) -> String {
+    let mut result = String::new();
+    let n = n as usize;
+    for i in 1..=n {
+        let spaces = " ".repeat(n - i);
+        let hashes = "#".repeat(i);
+        result.push_str(&spaces);
+        result.push_str(&hashes);
+        if i < n {
+            result.push('\n');
+        }
+    }
+    result
+}
+
+pub fn staircase(n: i32) {
+    println!("{}", staircase_logic(n));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_staircase_size_4() {
+        let expected = "   #\n  ##\n ###\n####";
+        assert_eq!(staircase_logic(4), expected);
+    }
+
+    #[test]
+    fn test_staircase_size_1() {
+        assert_eq!(staircase_logic(1), "#");
+    }
+}
+
