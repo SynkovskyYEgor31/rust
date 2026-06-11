384ddd91e64d5bcdee3aeaf7ded00eed9dcafc41
СинковскийЕгор31 <synkovsky.yegor@e-u.edu.ua>
2026-06-11 15:50:52 +0300

    Виконав практичну 9 і додав скріншоти

--- src/hackerrank/task10.rs
+++ src/hackerrank/task10.rs
@@ -1,1 +1,27 @@
-// https://www.hackerrank.com/challenges/sock-merchant/problem
+#![allow(dead_code)]
+
+pub fn sock_merchant(_n: i32, ar: &[i32]) -> i32 {
+    let mut counts = std::collections::HashMap::new();
+    for &sock in ar {
+        *counts.entry(sock).or_insert(0) += 1;
+    }
+    
+    let mut pairs = 0;
+    for &count in counts.values() {
+        pairs += count / 2;
+    }
+    
+    pairs
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_sock_merchant() {
+        let ar = vec![10, 20, 20, 10, 10, 30, 50, 10, 20];
+        assert_eq!(sock_merchant(9, &ar), 3);
+    }
+}
+
