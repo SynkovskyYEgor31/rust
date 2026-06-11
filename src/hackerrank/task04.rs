68ce5a6a646d2d7beb2e1343dfe286ed8852afb6
СинковскийЕгор31 <synkovsky.yegor@e-u.edu.ua>
2026-06-11 14:44:40 +0300

    скріншоти до приктичної роботи 3

--- src/hackerrank/task04.rs
+++ src/hackerrank/task04.rs
@@ -1,1 +1,31 @@
-// https://www.hackerrank.com/challenges/grading/problem
+#![allow(dead_code)]
+
+pub fn grading_students(grades: &[i32]) -> Vec<i32> {
+    let mut rounded_grades = Vec::new();
+    for &grade in grades {
+        if grade < 38 {
+            rounded_grades.push(grade);
+        } else {
+            let next_multiple = ((grade / 5) + 1) * 5;
+            if next_multiple - grade < 3 {
+                rounded_grades.push(next_multiple);
+            } else {
+                rounded_grades.push(grade);
+            }
+        }
+    }
+    rounded_grades
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_grading() {
+        let input = vec![73, 67, 38, 33];
+        let expected = vec![75, 67, 40, 33];
+        assert_eq!(grading_students(&input), expected);
+    }
+}
+
