3736b751f1a2f2a7bccc96cda02aa2c159b0b9ae
СинковскийЕгор31 <synkovsky.yegor@e-u.edu.ua>
2026-06-11 15:11:35 +0300

    Виконав практичну 5 і додав скріншоти

--- src/hackerrank/task06.rs
+++ src/hackerrank/task06.rs
@@ -1,1 +1,25 @@
-// https://www.hackerrank.com/challenges/kangaroo/problem
+#![allow(dead_code)]
+
+pub fn kangaroo(x1: i32, v1: i32, x2: i32, v2: i32) -> String {
+    if v1 <= v2 {
+        if x1 == x2 { "YES".to_string() } else { "NO".to_string() }
+    } else {
+        if (x2 - x1) % (v1 - v2) == 0 { "YES".to_string() } else { "NO".to_string() }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_kangaroo_meet() {
+        assert_eq!(kangaroo(0, 3, 4, 2), "YES");
+    }
+
+    #[test]
+    fn test_kangaroo_no_meet() {
+        assert_eq!(kangaroo(0, 2, 5, 3), "NO");
+    }
+}
+
